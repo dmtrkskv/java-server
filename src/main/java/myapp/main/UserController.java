e30966e69237bf9e65a513659bc4033b91f673f0
Your Name <you@example.com>
2020-02-08 00:24:08 +0300

    реализовал получение пользователя по id из Postgresql с помощью Hibernate

--- src/main/java/myapp/main/UserController.java
+++ src/main/java/myapp/main/UserController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import myapp.main.services.UserService;
+import myapp.main.models.User;
+
 @RestController
 public class UserController {
 
     @GetMapping("/getUser")
-    public User getUserInfo(@RequestParam(value = "firstName", required = false, defaultValue = "Iri") String firstName,
-            @RequestParam(value = "secondName", required = false, defaultValue = "Sergeeva") String secondName,
-            @RequestParam(value = "age", required = false, defaultValue = "20") int age) {
-        return new User(firstName, secondName, age);
+    public User getUserInfo(@RequestParam(value = "id", required = false, defaultValue = "1") int id) {
+        UserService userService = new UserService();
+        return userService.findUser(id);       
     }
 }
--- src/main/java/myapp/main/models/User.java
+++ src/main/java/myapp/main/models/User.java
@@ -3,10 +3,11 @@ package myapp.main.models;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users", schema = "public")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
     @Column(name = "name")
@@ -15,7 +16,15 @@ public class User {
     public User() {
     }
 
-    public User(String name) {
+    public User(String name) {   
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
     }
 }
